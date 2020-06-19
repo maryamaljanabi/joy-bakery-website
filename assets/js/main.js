5c66c0de1cde4ed70b47225bca831be463d93a17
maryamaljanabi <maryam.aljanabi135@gmail.com>
2020-06-19 21:33:20 +0300

    Add modals for cake page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
-  const productList = $(".product-container.bread > div > div");
-  productList.each((index, item) => {
+  const breadList = $(".product-container.bread > div > div");
+  breadList.each((index, item) => {
     $(item).click(function () {
       $("#breadModal".concat((index + 1).toString())).modal("show");
+    });
+  });
+
+  const cakeList = $(".product-container.cake > div > div");
+  cakeList.each((index, item) => {
+    $(item).click(function () {
+      $("#cakeModal".concat((index + 1).toString())).modal("show");
     });
   });
 });
